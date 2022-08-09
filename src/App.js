978f4a8d14fd42631b510ed9065b32fae77af9d2
nggoong <nggoong@naver.com>
2022-08-09 11:16:06 +0900

    백엔드(spring) 배포 후 소켓을 열어 통신 완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,108 @@
 import logo from './logo.svg';
-import Test from './nggoong/test';
+import Test from './nggoong/Test';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import SockJS from 'sockjs-client';
+import * as StompJS from "stompjs";
+import styled from 'styled-components';
 
 function App() {
+  const roomId = 0;
+  const [contents, setContents] = useState(null);
+  const [message, setMessage] = useState('');
+  const [number, setNumber] = useState(0);
+  const [receiv_msg, setReceiv_msg] = useState([]);
+  const [send_msg, setSend_msg] = useState([]);
+
+  const sock = new SockJS('http://3.37.89.214/life-tutor') // 바꾸기
+  const client = StompJS.over(sock);
+
+  const headers = {"temp":"temp"};
+
+  useEffect(()=> {
+    const connect = () => {
+    
+      client.connect({}, ()=> {
+        client.subscribe(`/sub/chat`, (data) => {
+          const newMessage = JSON.parse(data.body);
+          // setContents(newMessage);
+          // 상대방에게서 메세지를 받을 때 마다 실행됨
+          console.log(newMessage)
+          let new_data = receiv_msg.concat(newMessage.message);
+          setReceiv_msg(new_data);
+          console.log(newMessage.message);
+        })
+      })
+    }
+    connect();
+  }, [])
+
+  const disConnect = () => {
+    client.disconnect(() => {
+        client.unsubscribe()
+    });
+    // navigate('/home');
+  }
+
+  const sendMsg = (message) => {
+    client.send(
+      `/pub/receive`,
+      {},
+      // JSON.stringify({nickname, message})
+      JSON.stringify({"message":message})
+    );
+    setMessage("");
+    // setNumber(number+1);
+  };
+
+  const inputChange = (e) => {
+    setMessage(e.target.value);
+  }
+  const submithandler = (e) => {
+    // sendMsg('홍길동', message)
+    e.preventDefault();
+    sendMsg(message);
+    let new_data = [...send_msg, message];
+    setSend_msg(new_data);
+  }
+
+  
   return (
     <div className="App">
-     <Test></Test>
+      <input type="text" value={message} onChange={inputChange}></input>
+      <button onClick={submithandler}>submit</button>
+      <MsgWrapper>
+        <ReceivWrapper>
+         {receiv_msg.map((item, index) => <div key={index}>{item}</div>)}
+        </ReceivWrapper>
+        <SendWrapper>
+          {send_msg.map((item, index) => <div key={index}>{item}</div>)}
+        </SendWrapper>
+      </MsgWrapper>
     </div>
   );
 }
 
 export default App;
+
+
+const MsgWrapper = styled.div`
+  width:80vw;
+  height:80vh;
+  background:yellow;
+  display:flex;
+`
+
+
+const ReceivWrapper = styled.div`
+  width:50%;
+  height:100%;
+  background:gray;
+`
+
+const SendWrapper = styled(ReceivWrapper)`
+  background:lightgray;
+`
+
+
+
--- src/nggoong/Test.js
+++ src/nggoong/Test.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import SockJS from 'sockjs-client';
+import { Stomp } from '@stomp/stompjs';
 
-const Test = () => {
+let sockJS = new SockJS("http://localhost:8080/webSocket");
+let stompClient = Stomp.over(sockJS);
+stompClient.debug= () => {};
+
+const Test = ({ sockstate }) => {
+    const inputRef = React.useRef(null);
+    const [contents, setContents] = React.useState([]);
+    const [username, setUsername] = React.useState('홍길동');
+    const [message, setMessage] = React.useState('');
+
+    React.useEffect(()=> {
+        stompClient.connect({}, () => {
+            stompClient.subscribe('/topic/roomId', (data) => {
+                const newMessage = JSON.parse(data.body)
+            })
+        })
+    }, [])
+
+    
     return(
-        <>
-        
-        </>
+        <div className='container'>
+            <input type="text" ref={inputRef}></input>
+            <button>send</button>
+        </div>
     )
 }
 
